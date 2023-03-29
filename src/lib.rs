b69f7e2ea72728d19970a49fa86d5640cea79f32
Minjoon Hong <mjhong0708@naver.com>
2023-03-29 13:27:34 +0900

    Add quaternion-based RMSD & its gradient

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 pub mod logfermi;
+pub mod rmsd;
 
-use numpy::{IntoPyArray, PyArray2, PyReadonlyArray2};
+use crate::rmsd::compute_minimum_rmsd;
+use numpy::{IntoPyArray, PyArray1, PyArray2, PyReadonlyArray2};
 use pyo3::prelude::{pymodule, PyModule, PyResult, Python};
 
 #[pymodule]
@@ -16,6 +18,30 @@ fn ase_extension(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
         let positions = positions.as_array();
         let (e, e_grad) = logfermi::log_fermi(&positions, radius, temperature, beta);
         (e, e_grad.into_pyarray(_py))
+    }
+
+    #[pyfn(m)]
+    fn rmsd<'py>(
+        _py: Python<'py>,
+        positions_1: PyReadonlyArray2<f64>,
+        positions_2: PyReadonlyArray2<f64>,
+        compute_gradient: bool,
+    ) -> (
+        f64,
+        Option<&'py PyArray2<f64>>,
+        &'py PyArray2<f64>,
+        &'py PyArray1<f64>,
+    ) {
+        let positions_1 = positions_1.as_array();
+        let positions_2 = positions_2.as_array();
+        let result = compute_minimum_rmsd(&positions_1, &positions_2, compute_gradient);
+        let rmsd_grad = result.rmsd_grad.map(|x| x.into_pyarray(_py));
+        (
+            result.rmsd_val,
+            rmsd_grad,
+            result.rotation_matrix.into_pyarray(_py),
+            result.translation_vector.into_pyarray(_py),
+        )
     }
 
     Ok(())
